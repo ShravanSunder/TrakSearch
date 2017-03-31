0a6a10bcb701bafcd48ecb9e0d755f6db813b0ed
Shravan Sunder <shravan.sunder@firmex.com>
2017-03-31 17:18:17 -0400

    V1.07 check of DJ Trak Search

---
+++ TrakSearch/Id3Search/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Shravan.DJ.TagIndexer;
+using Shravan.DJ.TagIndexer.Data;
+
+namespace Id3Search
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		TagParser AllTagData = new TagParser();
+		SearchEngine Search = new SearchEngine();
+
+		public MainWindow()
+		{
+			try
+			{
+				InitializeComponent();
+
+				this.MusicData.ItemsSource = new List<Id3TagData>();
+				this.MusicData.Items.Refresh();
+			}
+			catch (Exception ex)
+			{
+				
+			}
+
+		}
+
+		private void SearchBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key==Key.Enter)
+			{
+				SearchMusic(SearchBox.Text);
+			}
+			else if (e.Key == Key.Escape)
+			{
+				SearchBox.Clear();
+				UpdateItemSource();
+			}
+		}
+
+		private void SearchMusic(string text)
+		{
+			var result = SearchEngine.Search(text);
+			UpdateItemSource(result);
+		}
+
+		private void UpdateItemSource(IEnumerable<Id3TagData> data = null)
+		{
+			data = data ?? AllTagData.tagList;
+
+			MusicData.ItemsSource = data;
+			MusicData.Items.Refresh();
+		}
+
+		private void SearchButton_Click(object sender, RoutedEventArgs e)
+		{
+			SearchMusic(SearchBox.Text);
+		}
+
+		private void CancelButton_Click(object sender, RoutedEventArgs e)
+		{
+			SearchBox.Clear();
+			UpdateItemSource();
+		}
+
+		private void MusicData_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			var data = (Id3TagData)MusicData.SelectedItem;
+			Clipboard.SetData(DataFormats.Text, data.Title ?? "" + "  " + data.Artist ?? "");
+		}
+
+		private void FolderButton_Click(object sender, RoutedEventArgs e)
+		{
+			FolderButton.IsEnabled = false;
+			Folder2Button.Visibility = Visibility.Hidden;
+			Folder2Button.IsEnabled = false;
+
+
+			var dialog = new WPFFolderBrowser.WPFFolderBrowserDialog("Select Music Folder");
+			var done = dialog.ShowDialog(this);
+			//AllTagData.IndexDirectory(@"H:\Zouk");
+
+			if (done == true)
+			{
+				var folder = dialog.FileName;
+
+				AllTagData.IndexDirectory(folder);
+
+				SearchEngine.AddUpdateLuceneIndex(AllTagData.tagList.AsEnumerable());
+				this.MusicData.ItemsSource = AllTagData.tagList;
+				this.MusicData.Items.Refresh();
+			}
+
+			FolderButton.IsEnabled = true;
+		}
+	}
+}
---
+++ TrakSearch/UnitTestProject1/IndexerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shravan.DJ.TagIndexer;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class IndexerTest
+	{
+		[TestMethod]
+		public void ReadTags()
+		{
+			var time = new Stopwatch();
+			time.Start();
+
+			var parser = new TagParser();
+
+			parser.IndexDirectory(@"H:\Zouk");
+		
+			//parser.IndexDirectory(@"F:\Shravan's Documents\Dropbox\!Backup\MkLinks\Mp3\Dance\Zouk");
+
+			//var mp3 = new TagParser(@"C:\Users\shravan\Dropbox\!Backup\MkLinks\Mp3\Dance\");
+			// var mp3 = new Mp3(@"C:\Users\shravan\Dropbox\!Backup\MkLinks\Mp3\Dance\Traktor-New Songs\");
+
+			//mp3.TryIndex();
+
+			Console.WriteLine(parser.tagList.Count);
+
+			Console.WriteLine(time.ElapsedMilliseconds);
+		}
+	}
+}

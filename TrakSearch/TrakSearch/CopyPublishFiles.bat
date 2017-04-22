9df3c4d7d169527bfd8ffbedd35e1fcb8248188e
Shravan <shravan.sunder@firmex.com>
2017-04-22 03:46:13 -0400

    publish bat not working

--- TrakSearch/TrakSearch/CopyPublishFiles.bat
+++ TrakSearch/TrakSearch/CopyPublishFiles.bat
@@ -1,3 +1,3 @@
-xcopy "publish" "%userprofile%\Dropbox\Share\DJ Trak Search\" /d/s/h/e/k/f/c/y
+xcopy "publish" "%userprofile%\Dropbox\Share\DJ Trak Search\" /s/h/e/k/f/c/y
 
-xcopy "publish" "%userprofile%\Dropbox\!Backup\MkLinks\Mp3\DJ Software\DJ Trak Search\" /d/s/h/e/k/f/c/y
+xcopy "publish" "%userprofile%\Dropbox\!Backup\MkLinks\Mp3\DJ Software\DJ Trak Search\" /s/h/e/k/f/c/y
